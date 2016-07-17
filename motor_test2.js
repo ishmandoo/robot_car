05cd0740a203359dda10955346cf3b645db30b2c
philzook58 <philip@FartMachine7.local>
2016-07-17 17:40:14 -0400

    motor test 2

---
+++ motor_test2.js
@@ -0,0 +1,27 @@
+
+var five = require("johnny-five");
+
+
+if (process.env.node_env == "production") {
+   var board = new five.Board({port: "/dev/ttyAMA0"});
+} else {
+   var board = new five.Board();
+}
+
+
+board.on("ready", function() {
+
+
+  motor = new five.Motor({
+    pins: {
+      pwm:9,
+      dir:8,
+      cdir: 7  
+    }
+  });
+
+  motor.start(255);
+  setTimeout( function(){motor.stop()} ,1000)
+
+
+});

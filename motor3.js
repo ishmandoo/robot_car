573313c95651e70d5db7edfcd4d5c0a00dd38b9d
philzook58 <philip@FartMachine7.local>
2016-07-17 18:05:29 -0400

    new motor test 3

---
+++ motor3.js
@@ -0,0 +1,31 @@
+
+
+var five = require("johnny-five");
+
+
+if (process.env.node_env == "production") {
+   var board = new five.Board({port: "/dev/ttyAMA0"});
+} else {
+   var board = new five.Board();
+}
+
+
+board.on("ready", function() {
+
+
+pwm = new five.Pin({
+  pin: 9
+});
+
+forward = new five.Pin({
+  pin: 8
+});
+
+
+  pwm.high()
+  forward.high()
+
+  setTimeout( function(){pwm.low(); forward.low()} ,1000)
+
+
+});
